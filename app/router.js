ee4a6fc35e7651d6996482bed10ccfa64270ca77
Nathan Otto <ottoetc@gmail.com>
2016-03-29 10:06:56 -0700

    initial commit without broken pairs script

---
+++ app/router.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('about');
+  this.route('contact');
+  this.route('announcements');
+  this.route('tenant');
+  this.route('rental');
+});
+
+export default Router;
